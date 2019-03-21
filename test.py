9c03232e447713004540c3cbda70b63a1a3d3a83
Steve Zhu <704242527zzh@gmail.com>
2019-03-20 22:20:06 -0400

    new

--- test.py
+++ test.py
@@ -1,4 +1,36 @@
-import numpy as np
-from calculate_dist import *
+int A[MAX],N;
+int partitions(int low,int high)
+{
+    int p=low,r=high,x=A[r],i=p-1;
+    for(int j=p;j<=r-1;j++)
+    {
+        if (A[j]<=x)
+        {
 
-a = np.exp(1.015)/(np.exp(1.015)+np.exp(0.985))
+            i=i+1;
+            swap(A[i],A[j]);
+        }
+    }
+    swap(A[i+1],A[r]);
+    return i+1;
+}
+int selection_algorithm(int left,int right,int kth)
+{
+    for(;;)
+    {
+        int pivotIndex=partitions(left,right);          //Select the Pivot Between Left and Right
+        int len=pivotIndex-left+1;
+
+        if(kth==len)
+            return A[pivotIndex];
+
+        else if(kth<len)
+            right=pivotIndex-1;
+
+        else
+        {
+            kth=kth-len;
+            left=pivotIndex+1;
+        }
+    }
+}
